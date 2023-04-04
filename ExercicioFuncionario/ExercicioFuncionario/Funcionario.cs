dacc480a0867b3a6ec048a480d353df81bb00450
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-04-04 14:45:01 -0300

    Nelio C# OOP: Programa para ler os dados de um funcionário e calcular salário

---
+++ ExercicioFuncionario/ExercicioFuncionario/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace ExercicioFuncionario
+{
+    class Funcionario
+    {
+
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        //Cálculos do salário
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public double AumentarSalario(double aumentoSalario)
+        {
+            double novoValor = SalarioBruto * (aumentoSalario / 100);
+            return novoValor += SalarioLiquido();
+        }
+
+        public override string ToString()
+        {
+            return "Funcionário: " + Nome + " | $";
+        }
+
+    }
+}
+
---
+++ ExercicioFuncionario/ExercicioFuncionario/Program.cs
@@ -0,0 +1,43 @@
+﻿using ExercicioFuncionario;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario funcionario = new Funcionario();
+
+            Console.WriteLine("Entre com os dados do funcionário abaixo:");
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Chama o método SalarioLiquido
+            double salarioLiq = funcionario.SalarioLiquido();
+
+            Console.WriteLine(funcionario + salarioLiq.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double aumentoSalarial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Atualiza o salário com base no método AumentarSalario
+            double novoSalario = funcionario.AumentarSalario(aumentoSalarial);
+
+            Console.WriteLine();
+            Console.WriteLine(funcionario + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+    }
+
+}
