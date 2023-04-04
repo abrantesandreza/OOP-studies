1a2ca073955a7e0649599b4978f2594bc08b658f
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-04-04 12:10:27 -0300

    Nelio C# OOP: Programa para calcular área, perímetro e diagonal de um retângulo

---
+++ ExercicioRetangulo/ExercicioRetangulo/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using ExercicioRetangulo;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            Console.Write("Largura: ");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Altura: ");
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaRetangulo = retangulo.Area();
+            double perimetroRetangulo = retangulo.Perimetro();
+            double diagonalRetangulo = retangulo.Diagonal();
+
+            Console.WriteLine();
+            Console.WriteLine("AREA = " + areaRetangulo.ToString("F2", CultureInfo.InvariantCulture) + " m²");
+            Console.WriteLine("PERÍMETRO = " + perimetroRetangulo.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonalRetangulo.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+    }
+
+}
---
+++ ExercicioRetangulo/ExercicioRetangulo/Retangulo.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ExercicioRetangulo
+{
+    class Retangulo
+    {
+
+        public double Largura;
+        public double Altura;
+
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (2 * Largura) + (2 * Altura); 
+        }
+
+        public double Diagonal()
+        {
+            double diagon = Math.Pow(Largura, 2) + Math.Pow(Altura, 2);
+            return Math.Sqrt(diagon);
+        }
+
+    }
+}
