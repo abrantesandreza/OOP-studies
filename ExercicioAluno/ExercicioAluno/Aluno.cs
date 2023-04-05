5ee96b153912043bf6245bb0757b0819a6b6ff54
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-04-04 21:31:27 -0300

    Nelio C# OOP: Programa principal construído e Classe Aluno construída

--- ExercicioAluno/ExercicioAluno/Aluno.cs
+++ ExercicioAluno/ExercicioAluno/Aluno.cs
@@ -1,10 +1,32 @@
-﻿
+﻿using System.Globalization
+
 namespace ExercicioAluno
 {
     class Aluno
     {
+        public string Nome;
+        public double NotaPrimeiroTri;
+        public double NotaSegundoTri;
+        public double NotaTerceiroTri;
 
 
+        public double NotaFinal() {
+            return NotaPrimeiroTri + NotaSegundoTri + NotaTerceiroTri
+        }
+
+        public string AprovadoOuReprovado() {
+            if (NotaFinal() > 60.0) {
+                return "APROVADO"
+            } else {
+                return "REPROVADO"
+            }
+        }
+
+        public double SeReprovado() {
+            if (AprovadoOuReprovado() == "REPROVADO") {
+                return 100 - NotaFinal()
+            }
+        }
 
     }
 }
--- ExercicioAluno/ExercicioAluno/Program.cs
+++ ExercicioAluno/ExercicioAluno/Program.cs
@@ -9,7 +9,23 @@ namespace Course
         static void Main(string[] args)
         {
 
-            
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite, abaixo, as três notas do aluno");
+            Console.Write("Primeiro Trimestre: ");
+            aluno.NotaPrimeiroTri = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Segundo Trimestre: ");
+            aluno.NotaSegundoTri = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Terceiro Trimestre: ");
+            aluno.NotaTerceiroTri = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(aluno.AprovadoOuReprovado());
 
         }
 
