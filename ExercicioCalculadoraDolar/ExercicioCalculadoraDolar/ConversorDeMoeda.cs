948a499aeb3dd1f4cba7c4df6df1ef43868a5a22
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-04-05 13:36:01 -0300

    Nelio C# OOP: Programa para converter dólar em real, com imposto

---
+++ ExercicioCalculadoraDolar/ExercicioCalculadoraDolar/ConversorDeMoeda.cs
@@ -0,0 +1,22 @@
+﻿namespace ExercicioCalculadoraDolar
+{
+    class ConversorDeMoeda
+    {
+
+        public double Cotacao;
+                
+        //Cálculos da conversão
+        public double ConverterDolarReal(double quantidadeDolar)
+        {
+            return Cotacao * quantidadeDolar;
+        }
+
+        public double ImpostoIOF(double quantidadeDolar)
+        {
+            double porcentImposto = (6.00 / 100.0) * ConverterDolarReal(quantidadeDolar);
+            double valorImposto = porcentImposto + ConverterDolarReal(quantidadeDolar);
+            return valorImposto;
+        }
+
+    }
+}
---
+++ ExercicioCalculadoraDolar/ExercicioCalculadoraDolar/Program.cs
@@ -0,0 +1,29 @@
+﻿using ExercicioCalculadoraDolar;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConversorDeMoeda conversor = new ConversorDeMoeda();
+
+            Console.Write("Qual a cotação do dólar: ");
+            conversor.Cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolarAComprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Chama o método com o cálculo final
+            double valorFinalComIOF = conversor.ImpostoIOF(dolarAComprar); 
+
+            Console.WriteLine("Valor a ser pago em reais: " + valorFinalComIOF.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+    }
+
+}
