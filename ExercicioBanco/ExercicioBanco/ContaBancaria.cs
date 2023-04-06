150eff4a29d9761d59fe5afd5d92d025b2175865
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-04-06 17:26:47 -0300

    Main: Sistema de criação/atualização de conta brancária

---
+++ ExercicioBanco/ExercicioBanco/ContaBancaria.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBanco
+{
+    class ContaBancaria
+    {
+
+        private int _numeroDaConta;
+        private string _nomeTitular;
+        private double _saldo;
+
+        
+        public ContaBancaria(int numeroDaConta, string nomeTitular, double valorDepositoInicial = 0.0)
+        {
+
+            _numeroDaConta = numeroDaConta;
+            _nomeTitular = nomeTitular;
+            _saldo = valorDepositoInicial;
+
+        }
+
+        public int NumeroDaConta
+        {
+            get { return _numeroDaConta;}
+        }
+
+        public string NomeTitular
+        {
+            get { return _nomeTitular;}
+            set 
+            { 
+                if (value != null && value.Length > 1)
+                {
+                    _nomeTitular = value;
+                }
+            }
+                
+        }
+        
+        public double Saldo
+        {
+            get { return _saldo; }
+        }
+
+        public void Depositar(double deposito)
+        {
+            _saldo += deposito;
+        }
+
+        public void Sacar(double valorSaque)
+        {
+            if (_saldo <= (valorSaque + 5.0))
+            {
+                Console.WriteLine("Saldo insuficiente para saque!");
+                Console.WriteLine("*Cobramos uma taxa de $5.00 para realizar a operação de saque.");
+
+            } else
+            {
+                _saldo -= valorSaque + 5.0; //desconto da taxa bancária
+
+            }
+        }            
+
+        public override string ToString()
+        {
+            return "Conta: " + NumeroDaConta
+                    + " | Titular: " + NomeTitular
+                    + " | Seu saldo atual: $" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+    
+}
---
+++ ExercicioBanco/ExercicioBanco/Program.cs
@@ -0,0 +1,61 @@
+﻿using ExercicioBanco;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Vamos criar a sua conta!");
+            Console.WriteLine();
+            Console.Write("Entre o número da sua conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char userResponse = char.Parse(Console.ReadLine());
+
+            //Valor default para o saldo
+            double valorDepositoInicial = 0.0;
+
+            //Caso exista valor depositado no momento da criação da conta
+            if (userResponse == 's' || userResponse == 'S')
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                valorDepositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            //Instanciando a classe ContaBancaria
+            ContaBancaria contaBancaria = new ContaBancaria(numeroConta, nomeTitular, valorDepositoInicial);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(contaBancaria);
+
+            //Depósito
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Depositar(valorDeposito);
+            Console.WriteLine(contaBancaria);
+
+            //Saque
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Sacar(valorSaque);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(contaBancaria);
+
+        }
+
+    }
+
+}
