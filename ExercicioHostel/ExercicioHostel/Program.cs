2d7f4dd677ffe11f443d882a5cbf0f75b22926df
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-04-11 10:31:50 -0300

    Nelio C# OOP: Programa para alugar quartos em um hostel

---
+++ ExercicioHostel/ExercicioHostel/Program.cs
@@ -0,0 +1,50 @@
+﻿using ExercicioHostel;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Renter[] rooms = new Renter[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int qntdQuarto = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= qntdQuarto; i++)
+            {
+                Console.WriteLine("Aluguel #" + i);
+                Console.Write("Nome: ");
+                string renterName = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string renterEmail = Console.ReadLine();
+
+                Console.Write("Quarto: ");
+                int renterRoom = int.Parse(Console.ReadLine());
+
+                //A entrada do usuário será o "ponteiro" para as informações do inquilino
+                rooms[renterRoom] = new Renter(renterName, renterEmail);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            
+            //Itera sobre o vector de Rooms para encontrar os ocupados
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                if (rooms[i] != null)
+                {
+                    Console.WriteLine(i + ": " + rooms[i]);
+                }
+            }
+
+        }
+
+    }
+
+}
